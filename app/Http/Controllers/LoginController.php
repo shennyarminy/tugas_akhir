0c42be80fe426d56878c58b4a4e05484006cb92b
shennyarminy <arminy.berliana@gmail.com>
2022-02-12 00:00:27 +0700

    Tugas_akhir

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+       $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required',
+        ]);
+
+        if(Auth::attempt($credentials)){
+            $request->only('email', 'password');
+            return redirect()-> intended('/home');
+        }
+    }
+}
---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,47 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <a href="index.html">Stisla</a>
+      </div>
+      
+      <ul class="sidebar-menu">
+        <li class="nav-item">
+          <a href="#" class="nav-link "><i class="fas fa-home"></i><span>Dashboard</span></a>
+        </li>  
+        <li class="menu-header">Master Data</li>
+          <li class="nav-item">
+            <a href="#" class="nav-link "><i class="fas fa-file"></i><span>Data Kriteria</span></a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link"><i class="fas fa-file-alt"></i><span>Data Sub Kriteria</span></a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link"><i class="fas fa-users"></i><span>Data Alternatif</span></a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link"><i class="fas fa-edit"></i><span>Data Penilaian</span></a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link"><i class="fas fa-calculator"></i><span>Data Perhitungan</span></a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link"><i class="fas fa-poll"></i><span>Data Hasil Akhir</span></a>
+          </li>
+
+          
+          <li class="menu-header">Master User</li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link has-dropdown"><i class="fas fa-users-cog"></i><span>Data User</span></a>
+            <ul class="dropdown-menu">
+              <li><a href="auth-forgot-password.html">Forgot Password</a></li>
+              <li><a href="login.blade.php">Login</a></li>
+              <li><a class="beep beep-sidebar" href="auth-login-2.html">Login 2</a></li>
+              <li><a href="auth-register.html">Register</a></li>
+              <li><a href="auth-reset-password.html">Reset Password</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link"> <i class="fas fa-user"></i> <span>Data Profil</span></a>
+          </li>
+    </aside>
+  </div>
